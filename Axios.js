ce7f0b68c336a8f15228af7b26c5476e2f6bbd43
Serhat Mercan <serhatmercan67@gmail.com>
2021-02-01 23:44:37 +0300

    Third-Party Libraries added.

--- Axios.js
+++ Axios.js
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
 // HTML Elements
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
@@ -5,38 +7,13 @@ const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
-function sendHttpRequest(method, url, data) {
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                return response.json();
-            } else {
-                return response.json().then(errData => {
-                    console.log(errData);
-                    throw new Error('Something went wrong - server-side.');
-                });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            throw new Error('Something went wrong!');
-        });
-}
-
 // Get Data
 async function fetchPosts() {
     try {
-        const responseData = await sendHttpRequest(
-            'GET',
+        const response = await axios.get(
             'https://jsonplaceholder.typicode.com/posts'
         );
-        const listOfPosts = responseData;
+        const listOfPosts = response.data;
         for (const post of listOfPosts) {
             const postEl = document.importNode(postTemplate.content, true);
             postEl.querySelector('h2').textContent = post.title.toUpperCase();
@@ -46,6 +23,7 @@ async function fetchPosts() {
         }
     } catch (error) {
         alert(error.message);
+        console.log(error.response);
     }
 }
 
@@ -58,17 +36,14 @@ async function createPost(title, content) {
         userId: userId
     };
 
-    // Using Form Data w/out Body: JSON Format & No Header    
-    // const fd = new FormData(form);
-    // fd.append('title', title);
-    // fd.append('body', content);
-    // fd.append('userId', userId);
-
-    sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+    const response = await axios.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        post
+    );
+    console.log(response);
 }
 
 fetchButton.addEventListener('click', fetchPosts);
-
 form.addEventListener('submit', event => {
     event.preventDefault();
     const enteredTitle = event.currentTarget.querySelector('#title').value;
@@ -81,9 +56,6 @@ form.addEventListener('submit', event => {
 postList.addEventListener('click', event => {
     if (event.target.tagName === 'BUTTON') {
         const postId = event.target.closest('li').id;
-        sendHttpRequest(
-            'DELETE',
-            `https://jsonplaceholder.typicode.com/posts/${postId}`
-        );
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     }
 });
---
+++ Lodash.js
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.17.20/lodash.min.js"></script>
+
+// Returns the New Array of Filtered Values
+_.difference([2, 1], [2, 3]); // => [1]
